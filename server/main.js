b312f3858f0c67d70d985c941b7deea7db1e2125
Bryan de Oliveira Brettas <brdebr.93@gmail.com>
2018-11-12 00:05:50 +0100

    Allow CORS

--- server/main.js
+++ server/main.js
@@ -24,6 +24,18 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(bodyParser.json());
 
+// Allow CORS
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+      'Access-Control-Allow-Methods',
+      'OPTIONS, GET, POST, PUT, PATCH, DELETE'
+    );
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+  });
+
 // PASSPORT
 app.use(passport.initialize());
 require('./config/passport')(passport);
