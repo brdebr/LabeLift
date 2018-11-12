bc06aaf5c00265d5986f8931b639793f58612c84
Bryan de Oliveira Brettas <brdebr.93@gmail.com>
2018-11-12 23:30:53 +0100

    First Decent UI

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
+import AppContainer from './containers/appContainer';
 
 class App extends Component {
   render() {
     return (
-      <div></div>
+      <AppContainer></AppContainer>
     );
   }
 }
