d7d6c78ed07c9d403d2a20e4abb73b65aed7748b
Bryan de Oliveira Brettas <brdebr.93@gmail.com>
2018-11-13 00:40:22 +0100

    Dispatch React build app
    Also changes default port for react dev server

--- server/main.js
+++ server/main.js
@@ -5,6 +5,8 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const passport = require('passport');
 
+const path = require('path');
+
 const sequelize = require('./database/sequelize');
 const initModels = require('./database/models');
 
@@ -23,6 +25,7 @@ app.use(bodyParser.urlencoded({
     extended: false
 }));
 app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, '/../frontend/build')));
 
 // Allow CORS
 
@@ -49,6 +52,10 @@ const userRoutes = require('./routes/user.routes');
 app.use('/api', userRoutes);
 app.use('/api', authRoutes);
 
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, '/../frontend/build', 'index.html'));
+  });
 
 // DATABASE AND START SERVER
 
