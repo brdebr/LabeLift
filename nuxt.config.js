7acd3cae249b8c5be393b00cd47e9609ae27250f
Bryan de Oliveira Brettas <brdebr.93@gmail.com>
2018-12-19 22:46:16 +0100

    Base decent Layout
    Remove all the useless buttons
    Format appbar and drawer
    Use custom transition for page changes

--- nuxt.config.js
+++ nuxt.config.js
@@ -60,13 +60,15 @@ module.exports = {
     ** You can extend webpack config here
     */
     extend(config, ctx) {
-      // Run ESLint on save
       if (ctx.isDev && ctx.isClient) {
         config.module.rules.push({
           enforce: 'pre',
           test: /\.(js|vue)$/,
           loader: 'eslint-loader',
-          exclude: /(node_modules)/
+          exclude: /(node_modules)/,
+          options: {
+            fix: true
+          }
         })
       }
     }
