eb777f7c1bea63c527019b5e04768c4a6db28b28
Bryan de Oliveira Brettas <brdebr.93@gmail.com>
2018-12-20 22:14:02 +0100

    Custome colors theme
    WIP, I'm no really convinced yet

--- plugins/vuetify.js
+++ plugins/vuetify.js
@@ -3,6 +3,15 @@ import Vuetify from 'vuetify'
 import colors from 'vuetify/es5/util/colors'
 
 Vue.use(Vuetify, {
+  theme: {
+    primary: colors.indigo.darken1,
+    secondary: colors.blue.darken2,
+    accent: colors.teal.accent4,
+    error: colors.red.darken3,
+    warning: colors.yellow.base,
+    info: colors.blue.base,
+    success: colors.lightGreen.accent4
+  },
   customProperties: true,
   iconfont: 'md'
 })
